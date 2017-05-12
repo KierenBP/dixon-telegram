9cbb495cc3e311eae573111d29c3494cceaf450d
Kieren <kieren@kieren.xyz>
2017-05-12 19:05:17 +1200

    Add weather feature. sends weather update when you send it your location

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const config = require('./config.json');
 const diceRoll = require('./commands/diceroll');
 const urbanDic = require('./commands/urbandic');
 const giphy = require('./commands/giphy');
+const weather = require('./commands/weather');
 
 
 const commands = {
   diceRoll,
   urbanDic,
   giphy,
+  weather,
 };
 
 // Token from Bot Father
@@ -30,6 +32,14 @@ bot.onText(/^(hello |hi |kia ora |hey )(!)?( dixon)?(!)?/ig, (msg) => {
     reply_to_message_id: msg.message_id,
     disable_notification: true,
   });
+});
+
+
+// Matches "/help"
+bot.onText(/\/help/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendChatAction(chatId, 'typing');
+  bot.sendMessage(chatId, 'Help! \nDixon Bot was created by @imkieren \n\nCommands and Actions:\n[word].gif - Searches giphy and returns a gif\n/urbandic [word] - Searches Urban Dictionary for specified word\n/roll (number) - Rolls dice. 6 sides is default.\nWeather Update - Gives you weather update when you *send your location*', { parse_mode: 'markdown' });
 });
 
 
@@ -66,6 +76,16 @@ bot.onText(/(.+)\.gif+$/g, (msg, match) => {
   bot.sendChatAction(chatId, 'upload_video');
   commands.giphy(match[1]).then((gifUrl) => {
     bot.sendVideo(chatId, gifUrl);
+  }).catch((err) => {
+    bot.sendMessage(chatId, `🚫Error! ${err}`);
+  });
+});
+
+bot.on('location', (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendChatAction(chatId, 'typing');
+  commands.weather(msg.location.latitude, msg.location.longitude).then((weatherInfo) => {
+    bot.sendMessage(chatId, weatherInfo);
   }).catch((err) => {
     bot.sendMessage(chatId, `🚫Error! ${err}`);
   });
